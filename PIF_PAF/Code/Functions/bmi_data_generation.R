a05629a10042cdafc90514b217d8e14cbfcd5d96
rfrost1 <reuben.j.frost@outlook.com>
2025-02-18 15:43:45 +0000

    Updating methodology

--- PIF_PAF/Code/Functions/bmi_data_generation.R
+++ PIF_PAF/Code/Functions/bmi_data_generation.R
@@ -152,21 +152,42 @@ bmi_data_gen <- function(filepath, user_options){
       ## Tabulate - depending on if imd is a needed variable or not
       
       if(user_options$imd_stratification){
+        
+        # Tabulate bmi
         ukds_data_temp_table <- ukds_data_output_temp |>
           filter(imd>=1) |>
           count(age_group, sex, bmi, imd, wt = weight) |>
           group_by(age_group, sex, imd) |>
-          mutate(value = n/sum(n),
-                 N = sum(n)) |>
+          mutate(value = n/sum(n)) |>
           select(-n) |>
           mutate(imd = as.character(imd))
+        
+        # Count participants per strata
+        ukds_data_temp_table_n <- ukds_data_output_temp |>
+          filter(imd>=1) |>
+          count(age_group, sex, imd) |>
+          rename(N = n) |>
+          mutate(imd = as.character(imd))
+        
+        # Now merge together
+        ukds_data_temp_table <- merge(ukds_data_temp_table, ukds_data_temp_table_n, by = c("age_group", "sex", "imd"))
+        
       } else {
+        
+        # Tabulate bmi
         ukds_data_temp_table <- ukds_data_output_temp |>
           count(age_group, sex, bmi, wt = weight) |>
           group_by(age_group, sex) |>
-          mutate(value = n/sum(n),
-                 N = sum(n)) |>
+          mutate(value = n/sum(n)) |>
           select(-n)
+        
+        # Count participants per strata
+        ukds_data_temp_table_n <- ukds_data_output_temp |>
+          count(age_group, sex) |>
+          rename(N = n)
+        
+        # Now merge together
+        ukds_data_temp_table <- merge(ukds_data_temp_table, ukds_data_temp_table_n, by = c("age_group", "sex"))
       }
       
       # Add year on
